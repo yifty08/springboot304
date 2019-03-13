7c2a1e0237a862dde95d1a000b59f366b0079f33
yifty08 <yifty08@icloud.com>
2019-03-13 14:10:21 -0400

    form, list, detail, update, delete

---
+++ src/main/java/com/example/springboot303/Springboot303Application.java
@@ -0,0 +1,15 @@
+package com.example.springboot303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot303Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Springboot303Application.class, args);
+
+	}
+
+}
